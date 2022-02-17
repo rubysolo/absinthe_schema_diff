7e6939ca736adc31fae3f7220d6a6a2249cf77be
Solomon White <rubysolo@gmail.com>
2022-02-17 16:44:49 -0700

    prep for docs generation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule AbsintheSchemaDiff.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Absinthe SchemaDiff",
+      source_url: "https://github.com/rubysolo/absinthe_schema_diff",
+      docs: [
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -20,6 +25,7 @@ defmodule AbsintheSchemaDiff.MixProject do
   defp deps do
     [
       {:absinthe, "~> 1.7.0"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:jason, "~> 1.3.0"},
       {:typed_struct, "~> 0.2.1"}
     ]
